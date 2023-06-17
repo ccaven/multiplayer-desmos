b83a08b62a0b6fe0f66988b70d9b04b057d2af8d
xacer <chacecaven@outlook.com>
2023-06-17 14:25:13 -0400

    update landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,60 +3,115 @@
     import { writable } from 'svelte/store';
     import { setContext } from 'svelte';
     import Networker from '$lib/Networker.svelte';
+    import { onMount } from 'svelte';
     let enabled = false;
-    let name = writable<string>();
+    let name = writable<string>("");
     setContext("player-name", name);
+
+    let goButton: HTMLButtonElement;
+
+    onMount(() => {
+        name.subscribe(newName => {
+            console.log(newName);
+            if (newName.length > 0) {
+                goButton.style.background = "#00aa1c";
+            } else {
+                goButton.style.background = "black";
+            }
+        });
+    });
+
+    function onClick() {
+        if ($name.length > 0) enabled = true;
+    }
+
 </script>
 
-<div>
-    
-    {#if enabled}
-        <Networker>
-            <Scene/>
-        </Networker>
-    {:else}
+{#if enabled}
+    <Networker>
+        <Scene/>
+    </Networker>
+{:else}
+    <div id="background">
         <div id="begin">
             <input type="text" placeholder="What's your name?" bind:value={$name}>
-            <button on:click={() => enabled=true}>
+            <button on:click={onClick} bind:this={goButton}>
                 {#if new URLSearchParams(location.search).has("join-id")}
                     Join Room
                 {:else}
-                    Host Room
+                    Create Graph
                 {/if}
             </button>    
         </div>
-    {/if}
-</div>
+    </div>
+{/if}
 
 <style>
-    :root(body) {
+    :global(body) {
         overflow: hidden;
         padding: 0;
         margin: 0;
+        transition: 0.1s ease;
+    }
+
+    #background {
+        width: 100vw;
+        height: 100vh;
+        top: 0;
+        left: 0;
+        position: absolute;
+        background-image: radial-gradient(#292929 10%, transparent 10%);
+        background-color: rgb(240, 240, 240);
+        background-position: 0 0, 5px 5px;
+        background-size: 15px 15px;
+        display: grid;
+        grid-template-columns: 1fr minmax(400px, 1fr) 1fr;
+        grid-template-rows: 1fr minmax(400px, 1fr) 1fr;
+        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
     }
 
     #begin {
-        display: grid;
-        grid-template-columns: auto;
-        grid-template-rows: 1fr 2fr;
-        padding: 25%;
+        border-radius: 25px;
+        grid-column: 2;
+        grid-row: 2;
         overflow: hidden;
-        border-radius: 50px;
+        background-color: rgba(250, 250, 250, 1.0);
+        box-shadow: 0px 0px 100px rgb(0 0 0 / 0.5);
     }
 
     #begin > input {
         width: 100%;
-        height: 2em;
+        height: 40%;
         font-size: x-large;
         text-align: center;
         padding: 0;
         margin: 0;
+        background: none;
+        border: none;
+        font-family: inherit;
+        transition: 0.5s ease;
+    }
+
+    #begin > input:focus {
+        outline-width: 0;
     }
 
     #begin > button {
         width: 100%;
+        height: 60%;
         font-size: 2em;
         padding: 0;
         margin: 0;
+        color: white;
+        border: none;
+        cursor: pointer;
+        font-family: inherit;
+        text-shadow: 0 0 10px rgb(0 0 0 / 0.5);
+        transition: 0.5s ease;
+    }
+
+    #begin > button:hover {
+        font-size: 2.5em;
+        text-shadow: 0 0 50px rgb(0 0 0 / 1);
     }
 </style>
