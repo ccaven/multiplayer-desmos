f5672098b7149946c2be56ed1d10aedf7a4f07e8
xacer <chacecaven@outlook.com>
2023-06-28 14:48:09 -0400

    fix paths

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
         paths: {
-            base: process.env.NODE_ENV === "production" ? "/code-together" : "",
+            base: process.env.NODE_ENV === "production" ? "/multiplayer-desmos" : "",
         },
 	}
 };
